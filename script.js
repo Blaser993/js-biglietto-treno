57685f2eb2aa43481f8888c3f8c7016681f4e6d0
Blaser993 <corradodepinto.design@gmail.com>
2023-01-31 14:43:12 +0100

    START

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("works");
+
+// numero di chilometri che vuole percorrere?
+// l’età del passeggero?
+
+let yourName = prompt("Scrivi il tuo nome")
+let yourSurame = prompt("Scrivi il tuo cognome")
+
+// prezzo totale del viaggio!
+
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+// - prezzo finale con massimo 2 decimali
