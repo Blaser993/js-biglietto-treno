6f1111bc0b8f9028efec303b6bfa60c7e94fc316
Blaser993 <corradodepinto.design@gmail.com>
2023-01-31 14:57:26 +0100

    NEW FEAT aggiunto variabile con costo del bigliett

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ console.log("works");
 // numero di chilometri che vuole percorrere?
 // l’età del passeggero?
 
-let yourName = prompt("Scrivi il tuo nome")
-let yourSurame = prompt("Scrivi il tuo cognome")
+let yourName = prompt("Scrivi il tuo nome");
+let yourSurname = prompt("Scrivi il tuo cognome");
+
+let yourDistance = prompt("Scrivi la distanza in Km che vuoi percorrere");
+
+console.log ("Biglietto di", yourName, yourSurname);
+console.log ("Per una tratta di", yourDistance, "Km");
 
 // prezzo totale del viaggio!
+
+const billPrize = (yourDistance * 0.21);
+console.log ("Al costo di", billPrize, "€");
 
 // - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // - va applicato uno sconto del 20% per i minorenni
