4bf1bfe5d6033614b98551af0caa9c2a32f834ad
Blaser993 <corradodepinto.design@gmail.com>
2023-01-31 15:04:39 +0100

    REFACT

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ let yourName = prompt("Scrivi il tuo nome");
 let yourSurname = prompt("Scrivi il tuo cognome");
 let yourAge = prompt("Scrivi la tua età")
 
+
+// prezzo totale del viaggio!
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 let yourDistance = prompt("Scrivi la distanza in Km che vuoi percorrere in Km");
+
+const billPrize = (yourDistance * 0.21);
 
 console.log ("Biglietto di", yourName, yourSurname);
 console.log ("Per una tratta di", yourDistance, "Km");
@@ -16,10 +21,9 @@ console.log ("Età del passeggero:", yourAge, "anni");
 
 
 
-// prezzo totale del viaggio!
-// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-const billPrize = (yourDistance * 0.21);
+
+
 
 
 // - va applicato uno sconto del 20% per i minorenni
